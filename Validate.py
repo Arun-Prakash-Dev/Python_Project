6617af598ed27770c2cf454037351f6b0b86ba27
Arun Prakash <69475166+Arun-Prakash-Dev@users.noreply.github.com>
2023-10-12 09:43:55 +0530

    Update Validate.py

--- Validate.py
+++ Validate.py
@@ -2,9 +2,22 @@ import re
 
 
 def validate_aadhaar(string):
-  return string.isdigit()
+  return len(string) and string.isdigit()
 
 
 def validate_pan(pan):
   pattern = r'^[A-Z]{5}[0-9]{4}[A-Z]{1}$'
   return re.match(pattern, pan) is not None
+
+
+def validate_DOB(date):
+  pattern = r'^\d{2}-\d{2}-\d{4}$'
+  return re.match(pattern, date) is not None
+
+
+def validate_accnt_type(accnt_type):
+  return accnt_type in ['savings', 'current']
+
+
+def validate_accnt_num(string):
+  return len(string) == 16 and string.isdigit()
